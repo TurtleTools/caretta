a2fea8a281f13655c68caaf4c1ebce4075e4ba19
Ninjani <>
2019-10-15 14:28:35 +0200

    dataclasses

--- caretta/neighbor_joining.py
+++ caretta/neighbor_joining.py
@@ -1,6 +1,8 @@
 import numba as nb
 import numpy as np
 
+from setup import numba_cc
+
 
 # Neighbor joining takes as input a distance matrix specifying the distance between each pair of taxa.
 # The algorithm starts with a completely unresolved tree, whose topology corresponds to that of a star network,
@@ -14,6 +16,7 @@ import numpy as np
 
 
 @nb.njit
+@numba_cc.export('neighbor_joining', '(f64[:])')
 def neighbor_joining(distance_matrix: np.ndarray) -> (np.ndarray, np.ndarray):
     """
     Runs the neighbor joining algorithm on a distance matrix
@@ -88,6 +91,7 @@ def neighbor_joining(distance_matrix: np.ndarray) -> (np.ndarray, np.ndarray):
 
 # Q matrix calculation + minimum i, j (step 1 & 2)
 @nb.njit
+@numba_cc.export('_find_join_nodes', '(f64[:])')
 def _find_join_nodes(distance_matrix):
     """
     Finds which nodes to join next
@@ -118,6 +122,7 @@ def _find_join_nodes(distance_matrix):
 
 # Branch length calculation (step 3)
 @nb.njit
+@numba_cc.export('_find_branch_length', '(f64[:], i64, i64)')
 def _find_branch_length(distance_matrix, i, j):
     """
     Finds branch lengths of old nodes to newly created node
--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from distutils.core import setup
 
-setup(name='caretta',
-      version='1.0',
-      authors=["Janani Durairaj", "Mehmet Akdel"],
-      packages=["caretta"],
-      install_requires=["numpy", "numba", "prody", "biopython", "fire"])
+from numba.pycc import CC
+
+numba_cc = CC('caretta')
+numba_cc.verbose = True
+
+
+def main():
+      setup(name='caretta',
+            version='1.0',
+            authors=["Janani Durairaj", "Mehmet Akdel"],
+            packages=["caretta"],
+            install_requires=["numpy", "numba", "prody", "biopython", "fire"],
+            ext_modules=[numba_cc.distutils_extension()])
+
+
+if __name__ == '__main__':
+      main()
