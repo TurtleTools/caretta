8a2fb43876c113c6a4d1391d43dac1e4a29d6cb0
Ninjani <>
2019-10-16 13:13:53 +0200

    added app and improved readme

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(name='caretta',
       version='1.0',
       authors=["Janani Durairaj", "Mehmet Akdel"],
       packages=["caretta"],
-      install_requires=["numpy", "numba", "prody", "biopython", "fire", "dash==1.3.1", "dash-bio==0.1.4",
-                        "dash-core-components==1.2.1", "dash-html-components==1.0.1", "dash-renderer==1.1.0",
-                        "dash-table==4.3.0", "plotly==3.7.1", "flask"])
+      install_requires=["numpy", "numba", "prody", "biopython", "fire"],
+      extras_require={
+          'GUI': ["dash==1.3.1", "dash-bio==0.1.4",
+                  "dash-core-components==1.2.1", "dash-html-components==1.0.1", "dash-renderer==1.1.0",
+                  "dash-table==4.3.0", "plotly==3.7.1", "flask"]
+      })
