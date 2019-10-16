782b14c08d47d6a25fc7216e757ea55d15941648
Akdel <mehmet_akdel@hotmail.com>
2019-10-16 15:52:54 +0200

    Merge branches 'doc' and 'master' of https://git.wur.nl/durai001/caretta
    # Conflicts:
    #	bin/caretta-app-demo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='caretta',
       packages=["caretta"],
       install_requires=["numpy", "numba", "prody", "biopython", "fire"],
       extras_require={
-          'GUI': ["dash==1.3.1", "dash-bio==0.1.4",
+          'GUI': ["dash==1.3.1", "dash-bio==0.1.4", "cryptography",
                   "dash-core-components==1.2.1", "dash-html-components==1.0.1", "dash-renderer==1.1.0",
                   "dash-table==4.3.0", "plotly==3.7.1", "flask"]
       })
