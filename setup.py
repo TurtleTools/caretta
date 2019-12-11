47503508e8e08767997c06bdf7a6d6be2c217608
Durairaj, Janani <janani.durairaj@wur.nl>
2019-12-11 11:36:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='caretta',
       version='1.0',
       authors=["Janani Durairaj", "Mehmet Akdel"],
       packages=["caretta"],
-      install_requires=["numpy==1.16.4", "numba==0.44.1", "scipy==1.3.0", "prody==1.10.10", "biopython==1.74", "fire==0.2.1", "pyparsing==2.4.0"],
+      install_requires=["numpy==1.16.2", "numba==0.43.0", "scipy==1.3.1", "prody==1.10.10", "biopython==1.74", "fire==0.2.1", "pyparsing==2.4.2"],
       extras_require={
           'GUI': ["dash==1.3.1", "dash-bio==0.1.4", "cryptography",
                   "dash-core-components==1.2.1", "dash-html-components==1.0.1", "dash-renderer==1.1.0",
