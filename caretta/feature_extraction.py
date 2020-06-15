18664a0d9f55bd431a7a069c757968ffed11276d
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-06-15 13:01:31 +0200

    removing caretta helper and replacing with geometricus utility

--- caretta/feature_extraction.py
+++ caretta/feature_extraction.py
@@ -3,9 +3,55 @@ from pathlib import Path
 
 import numpy as np
 import prody as pd
+import Bio.PDB
 from Bio.PDB.ResidueDepth import get_surface, residue_depth, ca_depth, min_dist
+from geometricus import utility
 
-from caretta import helper
+
+def read_pdb(input_file, name: str = None, chain: str = None) -> tuple:
+    """
+    returns protein information from PDB file
+    Parameters
+    ----------
+    input_file
+    name
+        None => takes from filename
+    chain
+        only for that chain
+
+    Returns
+    -------
+    structure Object, list of residue Objects, list of peptide Objects, sequence, sequence to residue index
+    """
+    if name is None:
+        (_, name, _) = utility.get_file_parts(input_file)
+    input_file = str(input_file)
+    structure = Bio.PDB.PDBParser().get_structure(name, input_file)
+    if chain is not None:
+        structure = structure[0][chain]
+    residues = Bio.PDB.Selection.unfold_entities(structure, 'R')
+    peptides = Bio.PDB.PPBuilder().build_peptides(structure)
+    sequence = ''.join([str(peptide.get_sequence()) for peptide in peptides])
+    residue_dict = dict(zip(residues, range(len(residues))))
+    seq_to_res_index = [residue_dict[r] for peptide in peptides for r in peptide]
+    return structure, residues, peptides, sequence, seq_to_res_index
+
+
+def get_alpha_coordinates(residue) -> np.ndarray:
+    """
+    Returns alpha coordinates of BioPython residue object
+    """
+    return np.array(residue['CA'].get_coord())
+
+
+def get_beta_coordinates(residue) -> np.ndarray:
+    """
+    Returns beta coordinates of BioPython residue object
+    (alpha if Gly)
+    """
+    if residue.get_resname() == "GLY" or 'CB' not in residue:
+        return get_alpha_coordinates(residue)
+    return np.array(residue['CB'].get_coord())
 
 
 def get_residue_depths(pdb_file):
@@ -20,10 +66,10 @@ def get_residue_depths(pdb_file):
     -------
     dict of depth _ ca/cb/mean
     """
-    structure, residues, _, _, _ = helper.read_pdb(pdb_file)
+    structure, residues, _, _, _ = read_pdb(pdb_file)
     surface = get_surface(structure)
     data = {"depth_mean": np.array([residue_depth(residue, surface) for residue in residues]),
-            "depth_cb": np.array([min_dist(helper.get_beta_coordinates(residue), surface) for residue in residues]),
+            "depth_cb": np.array([min_dist(get_beta_coordinates(residue), surface) for residue in residues]),
             "depth_ca": np.array([ca_depth(residue, surface) for residue in residues])}
     return data
 
@@ -42,8 +88,8 @@ def get_fluctuations(protein: pd.AtomGroup, n_modes: int = 50):
     dict of anm_ca, anm_cb, gnm_ca, gnm_cb
     """
     data = {}
-    beta_indices = helper.get_beta_indices(protein)
-    alpha_indices = helper.get_alpha_indices(protein)
+    beta_indices = utility.get_beta_indices(protein)
+    alpha_indices = utility.get_alpha_indices(protein)
     data["anm_cb"] = get_anm_fluctuations(protein[beta_indices], n_modes)
     data["gnm_cb"] = get_gnm_fluctuations(protein[beta_indices], n_modes)
     data["anm_ca"] = get_anm_fluctuations(protein[alpha_indices], n_modes)
@@ -110,7 +156,7 @@ def get_features(pdb_file: str, dssp_dir: str, only_dssp=True, force_overwrite=T
     dict of features
     """
     pdb_file = str(pdb_file)
-    _, name, _ = helper.get_file_parts(pdb_file)
+    _, name, _ = utility.get_file_parts(pdb_file)
     protein = pd.parsePDB(pdb_file).select("protein")
     # if Path(pdb_file).suffix != ".pdb":
     pdb_file = str(Path(dssp_dir) / f"{name}.pdb")
@@ -166,7 +212,7 @@ def get_dssp_features(protein_dssp):
     dssp_ignore = ["dssp_bp1", "dssp_bp2", "dssp_sheet_label", "dssp_resnum"]
     dssp_labels = [label for label in protein_dssp.getDataLabels() if label.startswith("dssp") and label not in dssp_ignore]
     data = {}
-    alpha_indices = helper.get_alpha_indices(protein_dssp)
+    alpha_indices = utility.get_alpha_indices(protein_dssp)
     indices = [protein_dssp[x].getData("dssp_resnum") for x in alpha_indices]
     for label in dssp_labels:
         label_to_index = {i - 1: protein_dssp[x].getData(label) for i, x in zip(indices, alpha_indices)}
