2b19a8f2e8141ed731ac6f328f240d2cbb1a6a10
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-06-15 13:00:23 +0200

    renaming for clarity

--- caretta/dynamic_time_warping.py
+++ caretta/dynamic_time_warping.py
@@ -1,12 +1,10 @@
 import numba as nb
 import numpy as np
+MIN_FLOAT64 = np.finfo(np.float64).min
 
-MINV = np.finfo(np.float64).min
 
-
-@nb.njit
-# @numba_cc.export('_make_dtw_matrix', 'f64[:](f64[:], f64, f64)')
-def _make_dtw_matrix(distance_matrix: np.ndarray,
+@nb.njit(cache=False)
+def _make_dtw_matrix(score_matrix: np.ndarray,
                      gap_open_penalty: float = 0.,
                      gap_extend_penalty: float = 0.):
     """
@@ -14,8 +12,8 @@ def _make_dtw_matrix(distance_matrix: np.ndarray,
 
     Parameters
     ----------
-    distance_matrix
-        matrix of distances between corresponding vectors of the two vector sets; shape = (n, m)
+    score_matrix
+        matrix of scores between corresponding vectors of the two vector sets; shape = (n, m)
     gap_open_penalty
         penalty for opening a (series of) gap(s)
     gap_extend_penalty
@@ -27,20 +25,20 @@ def _make_dtw_matrix(distance_matrix: np.ndarray,
     """
     gap_open_penalty *= -1
     gap_extend_penalty *= -1
-    n, m = distance_matrix.shape
+    n, m = score_matrix.shape
     matrix = np.zeros((n + 1, m + 1, 3), dtype=np.float64)
-    matrix[:, 0, :] = MINV
-    matrix[0, :, :] = MINV
+    matrix[:, 0, :] = MIN_FLOAT64
+    matrix[0, :, :] = MIN_FLOAT64
     matrix[0, 0] = 0
     backtrack = np.zeros((n + 1, m + 1, 3), dtype=np.int64)
     for i in range(1, n + 1):
         matrix[i, 0, 0] = 0
         matrix[i, 0, 1] = 0
-        matrix[i, 0, 2] = MINV - gap_open_penalty
+        matrix[i, 0, 2] = MIN_FLOAT64 - gap_open_penalty
         backtrack[i, 0] = 0
 
     for j in range(1, m + 1):
-        matrix[0, j, 0] = MINV - gap_open_penalty
+        matrix[0, j, 0] = MIN_FLOAT64 - gap_open_penalty
         matrix[0, j, 1] = 0
         matrix[0, j, 2] = 0
         backtrack[0, j] = 1
@@ -49,30 +47,26 @@ def _make_dtw_matrix(distance_matrix: np.ndarray,
         for j in range(1, m + 1):
             scores_lower = np.array([matrix[i - 1, j, 0] + gap_extend_penalty,
                                      matrix[i - 1, j, 1] + gap_open_penalty])
-            min_index_lower = np.argmax(scores_lower)
-            min_value_lower = scores_lower[min_index_lower]
-            matrix[i, j, 0] = min_value_lower
-            backtrack[i, j, 0] = min_index_lower
+            index_lower = np.argmax(scores_lower)
+            matrix[i, j, 0] = scores_lower[index_lower]
+            backtrack[i, j, 0] = index_lower
 
             scores_upper = np.array([matrix[i, j - 1, 1] + gap_open_penalty,
                                      matrix[i, j - 1, 2] + gap_extend_penalty])
-            min_index_upper = np.argmax(scores_upper)
-            min_value_upper = scores_upper[min_index_upper]
-            matrix[i, j, 2] = min_value_upper
-            backtrack[i, j, 2] = min_index_upper + 1
+            index_upper = np.argmax(scores_upper)
+            matrix[i, j, 2] = scores_upper[index_upper]
+            backtrack[i, j, 2] = index_upper + 1
 
             scores = np.array([matrix[i, j, 0],
-                               matrix[i - 1, j - 1, 1] + distance_matrix[i - 1, j - 1],
+                               matrix[i - 1, j - 1, 1] + score_matrix[i - 1, j - 1],
                                matrix[i, j, 2]])
-            min_index = np.argmax(scores)
-            min_value = scores[min_index]
-            matrix[i, j, 1] = min_value
-            backtrack[i, j, 1] = min_index
+            index = np.argmax(scores)
+            matrix[i, j, 1] = scores[index]
+            backtrack[i, j, 1] = index
     return matrix, backtrack
 
 
-@nb.njit
-# @numba_cc.export('_get_dtw_alignment', '(i64, i64[:], i64, i64)')
+@nb.njit(cache=False)
 def _get_dtw_alignment(start_direction, backtrack: np.ndarray, n1, m1):
     """
     Finds optimal warping path from a backtrack matrix
@@ -127,20 +121,18 @@ def _get_dtw_alignment(start_direction, backtrack: np.ndarray, n1, m1):
                 indices_1[index] = -1
                 indices_2[index] = m
                 index += 1
-
     return indices_1[:index][::-1], indices_2[:index][::-1]
 
 
 @nb.njit
-# @numba_cc.export('dtw_align', '(f64[:], f64, f64)')
-def dtw_align(distance_matrix: np.ndarray, gap_open_penalty: float = 0., gap_extend_penalty: float = 0.):
+def dtw_align(score_matrix: np.ndarray, gap_open_penalty: float = 0., gap_extend_penalty: float = 0.):
     """
     Align two objects using dynamic time warping
 
     Parameters
     ----------
-    distance_matrix
-        (n x m) matrix of distances between all points of both objects
+    score_matrix
+        (n x m) matrix of scores between all points of both objects
     gap_open_penalty
         penalty for opening a (series of) gap(s)
     gap_extend_penalty
@@ -149,9 +141,9 @@ def dtw_align(distance_matrix: np.ndarray, gap_open_penalty: float = 0., gap_ext
     -------
     aligned_indices_1, aligned_indices_2
     """
-    matrix, backtrack = _make_dtw_matrix(distance_matrix, gap_open_penalty, gap_extend_penalty)
-    n = distance_matrix.shape[0]
-    m = distance_matrix.shape[1]
+    matrix, backtrack = _make_dtw_matrix(score_matrix, gap_open_penalty, gap_extend_penalty)
+    n = score_matrix.shape[0]
+    m = score_matrix.shape[1]
     scores = np.array([matrix[n, m, 0], matrix[n, m, 1], matrix[n, m, 2]])
     index = np.argmax(scores)
     aln_1, aln_2 = _get_dtw_alignment(index, backtrack, n, m)
